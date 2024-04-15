7b06bf267e7e9772499a7f453e08df166d44b8b9
roberto Guagliardo <administratie@sitiweb.nl>
2024-04-15 16:56:58 +0200

    Update met alles erop en eraan

--- classes/bookly-payment.php
+++ classes/bookly-payment.php
@@ -4,9 +4,11 @@ class SW_Bookly_Payment{
 
     private $payment_id;
     public $payment;
+    private $data;
 
     public function __construct($payment_id)
     {
+        $this->data = [];
         $this->payment_id = $payment_id;
     }
 
@@ -79,6 +81,10 @@ class SW_Bookly_Payment{
     }
 
     public function get_appointment_data(){
+
+        if ($this->data){
+            return $this->data;
+        }
         global $wpdb;
         $table_name_payments = $wpdb->prefix . 'bookly_payments';
         $updated_row = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_payments WHERE ref_id = %s", $this->payment_id), ARRAY_A);
@@ -94,15 +100,46 @@ class SW_Bookly_Payment{
             
             // Now, perform a second query using $payment_id.
             $data['appointment'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name WHERE payment_id = %d", $payment_id), ARRAY_A);
+            if ($data['appointment'] === false){
+                error_log('Appointment not found');
+                return false;
+            }
     
-            $data['customer_appointment'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_appointment WHERE id = %d", $appointment['appointment_id']), ARRAY_A);
+            $data['customer_appointment'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_appointment WHERE id = %d",  $data['appointment']['appointment_id']), ARRAY_A);
        
-            $data['customer'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_customer WHERE id = %d", $appointment['customer_id']), ARRAY_A);
+            $data['customer'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_customer WHERE id = %d",  $data['appointment']['customer_id']), ARRAY_A);
             
-            $data['service'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_service WHERE id = %d", $b_appointment['service_id']), ARRAY_A);
+            $data['service'] = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_service WHERE id = %d",  $data['appointment']['service_id']), ARRAY_A);
+            $this->data = $data;
         }
-        return $data;
+        
+        return $this->data;
     }
 
+    public function set_status($new_status){
+        global $wpdb;
+        $table_name_payments = $wpdb->prefix . 'bookly_payments'; // Replace 'your_table_name' with the actual table name
+        
+        // Retrieve the current status from the database
+        $row_to_update = $wpdb->get_row($wpdb->prepare("SELECT * FROM $table_name_payments WHERE ref_id = %s", $this->payment_id), ARRAY_A);
+    
+        // Check if the row exists and the current status is different from the new status
+        if ($row_to_update && $row_to_update['status'] !== $new_status) {
+                $this->get_appointment_data();
+                $wpdb->update(
+                    $table_name_payments,
+                    array('status' => $new_status), // Data to update
+                    array('ref_id' => $this->payment_id), // Where clause
+                    array('%s'), // Data format
+                    array('%d') // Where format
+                );
+        
+                // Send email only if the status is changed
+                (new sw_bookly_email)->update_payment_info('zaandam@bodyunlimited.nl', $new_status, $customer_data, $appointment, $service_data);
+                (new sw_bookly_email)->update_payment_info($customer_data->email, $new_status, $customer_data, $appointment, $service_data);
+            
+        }
+    }
+    
     
 }
